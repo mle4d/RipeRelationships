2cb4f04b96fb516dcae1d13c85bbc1e0f5aa8cda
emily <mle4d@me.com>
2019-07-18 16:35:16 -0700

    passing film tests and routes done

--- test/film.test.js
+++ test/film.test.js
@@ -4,8 +4,11 @@ const request = require('supertest');
 const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
+const Film = require('../lib/models/Film');
+const Studio = require('../lib/models/Studio');
+const Actor = require('../lib/models/Actor');
 
-describe('app routes', () => {
+describe('film tests', () => {
   beforeAll(() => {
     connect();
   });
@@ -14,7 +17,101 @@ describe('app routes', () => {
     return mongoose.connection.dropDatabase();
   });
 
+  let studio = null;
+  let actor = null;
+  beforeEach(async() => {
+    studio = JSON.parse(JSON.stringify(await Studio.create({ name: 'Miramax Films' })));
+    actor = JSON.parse(JSON.stringify(await Actor.create({ name: 'Johnny Depp' })));
+  });
+
+
   afterAll(() => {
     return mongoose.connection.close();
   });
+
+  it('creates film', () => {
+    return request(app)
+      .post('/api/v1/film')
+      .send({ 
+        title: 'Dead Man', 
+        studio: studio._id, 
+        release: 1996,
+        cast: [{ 
+          actor: actor._id,
+          role: 'William Blake' }]
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          title: 'Dead Man',
+          studio: studio._id,
+          release: 1996,
+          cast: [{
+            _id: expect.any(String),
+            actor: actor._id,
+            role: 'William Blake' }],
+          __v: 0
+        });
+      });
+  });
+  
+  it('gets film', async() => {
+    const film = await Film.create([
+      { title: 'Dead Man',
+        studio: studio._id,
+        release: 1996
+      }
+
+    ]);
+
+    return request(app)
+      .get('/api/v1/film')
+      .then(res => {
+        const filmsJSON = JSON.parse(JSON.stringify(film));
+        filmsJSON.forEach(film => {
+          expect(res.body).toContainEqual({ 
+            title: film.title, 
+            _id: film._id });
+        });
+      });
+  });
+  it('can delete an film by id', async() => {
+    const film = await Film.create({
+      title: 'Dead Man',
+      studio: studio._id,
+      release: 1996
+    });
+        
+    return request(app)
+      .delete(`/api/v1/film/${film._id}`)
+      .then(res => {
+        const filmJSON = JSON.parse(JSON.stringify(film));
+        expect(res.body).toEqual({ 
+          title: 'Dead Man', 
+          _id: filmJSON._id });
+      });
+  });
 });
+    
+
+
+// it('gets film by id', async() => {
+//   const studio = await Film.create({
+//     title: 'Dead Man',
+//     studio: studio._id,
+//     release: 1996
+//   });
+
+//   return request(app)
+//     .get(`/api/v1/film/${film._id}`)
+//     .then(res => {
+//       const filmJSON = JSON.parse(JSON.stringify(film));
+//       expect(res.body).toEqual({
+//         ...filmJSON,
+//         films: [{
+//          _id: expect.any(String),
+//          title: expect.any(String), 
+//          release: expect.any(String) 
+//        }]
+//       });
+//     });
